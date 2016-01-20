5c21d4fb178881deb90b7c6cf14e05475516eb15
davidshiyang <david.liu@viacom.com>
2016-01-20 16:47:27 -0500

    first teleportation push
    throwing grenades to teleport

---
+++ Assets/Scripts/grenadeLauncher.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grenadeLauncher : MonoBehaviour {
+
+    public GameObject grenade;
+    public GameObject throwArm;
+    public float xforce = 1f;
+
+
+    private Vector3 throwForce;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnGrenade();
+        }
+
+	}
+
+    void SpawnGrenade()
+    {
+        throwForce = (Camera.main.transform.forward) * xforce;
+        GameObject newGrenade = (GameObject)Instantiate(grenade, throwArm.transform.position, Quaternion.identity);
+        newGrenade.GetComponent<Rigidbody>().AddForce(throwForce);
+    }
+}
---
+++ Assets/Scripts/teleportationController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleportationController : MonoBehaviour {
+
+    public GameObject playerGO;
+    public float dropSpeed = 3f;
+    public float transportOffsetX = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Floor")
+        {
+            playerGO.transform.position = this.transform.position;
+            Destroy(this.gameObject);
+        }
+    }
+}
