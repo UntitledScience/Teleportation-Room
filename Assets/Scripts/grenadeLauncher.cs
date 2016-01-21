b9bb738d026f052932410d47ec503bbb3131b9ab
davidshiyang <david.liu@viacom.com>
2016-01-21 14:10:47 -0500

    added variable throw strength

--- Assets/Scripts/grenadeLauncher.cs
+++ Assets/Scripts/grenadeLauncher.cs
@@ -5,15 +5,22 @@ public class grenadeLauncher : MonoBehaviour {
 
     public GameObject grenade;
     public GameObject throwArm;
-    public float xforce = 1f;
+    public GameObject throwArmMax;
+    public Camera thisCamera;
 
+    public float maxForce = 1f;
+    public float rateForce = 1.3f;
+    public float originalForce = 100f;
 
+    private float curForce = 1f;
     private Vector3 throwForce;
+    private bool preppingThrow = false;
+
+    private GameObject tempGrenade;
 
 	// Use this for initialization
 	void Start () {
-
-
+        curForce = originalForce;
 	}
 	
 	// Update is called once per frame
@@ -21,15 +28,57 @@ public class grenadeLauncher : MonoBehaviour {
 	
         if (Input.GetButtonDown("Fire1"))
         {
-            SpawnGrenade();
+            preppingThrow = true;
+            GameObject newGrenade = (GameObject)Instantiate(grenade, throwArm.transform.position, Quaternion.identity);
+            tempGrenade = newGrenade;
+            tempGrenade.GetComponent<Rigidbody>().isKinematic = true;
+            tempGrenade.transform.parent = thisCamera.transform;
+            StartCoroutine(PrepGrenade());
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            preppingThrow = false;
+          SpawnGrenade();
         }
 
 	}
 
+    IEnumerator PrepGrenade()
+    {
+        while (preppingThrow)
+        {
+            // Add force to private force variable
+            if (curForce < maxForce)
+            {
+                curForce = curForce * rateForce;
+                SetPrepGrenadeTransform();
+               // print(curForce);
+            } else
+            {
+                yield break;
+            }
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    void SetPrepGrenadeTransform()
+    {
+        // Get distance between two Vectors (throwArm and throwArmMax)
+        float percent = curForce / maxForce;
+        Vector3 dist = Vector3.Lerp(throwArm.transform.position, throwArmMax.transform.position, percent);
+
+        // Set transform of tempGrenade as a percentage between two vectors.
+        tempGrenade.transform.position = dist;
+    }
+
     void SpawnGrenade()
     {
-        throwForce = (Camera.main.transform.forward) * xforce;
-        GameObject newGrenade = (GameObject)Instantiate(grenade, throwArm.transform.position, Quaternion.identity);
-        newGrenade.GetComponent<Rigidbody>().AddForce(throwForce);
+        // GameObject newGrenade = (GameObject)Instantiate(grenade, throwArm.transform.position, Quaternion.identity);
+        tempGrenade.GetComponent<Rigidbody>().isKinematic = false;
+        tempGrenade.transform.parent = null;
+        throwForce = (thisCamera.transform.forward) * curForce;
+        tempGrenade.GetComponent<Rigidbody>().AddForce(throwForce);
+        curForce = originalForce; // reset curForce
     }
 }
